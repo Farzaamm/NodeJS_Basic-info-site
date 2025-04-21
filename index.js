0f91f0c8f16198e5f540dd905be646005dd9f347
Farzaamm <93753345+Farzaamm@users.noreply.github.com>
2025-04-21 13:22:26 +0800

    NodeJS Basic Info Site Project

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const fs = require('fs');
+
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    if(req.url === '/') {
+        fs.readFile('./views/index.html', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.end();
+            } else {
+                res.end(data);
+            }
+        });
+    }else if(req.url === '/about') {
+        fs.readFile('./views/about.html', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.end();
+            } else {
+                res.end(data);
+            }
+        });
+    }else if(req.url === '/contact-me') {
+        fs.readFile('./views/contact-me.html', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.end();
+            } else {
+                res.end(data);
+            }
+        });
+    }else {
+        fs.readFile('./views/404.html', (err, data) => {
+            if (err) {
+                console.error(err);
+                res.end();
+            } else {
+                res.end(data);
+            }
+        });
+    }
+}).listen(8080);
+
+console.log('Server running at http://127.0.0.1:8080/'); 
